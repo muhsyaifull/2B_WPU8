4162362e5087e1f5115496b8b8c8763957ceca15
MUHAMMAD SYAIFULLAH <mahkotonagata@gmail.com>
2023-11-07 22:32:11 +0700

    <edit> input gap

--- app/Models/Pendidikan.php
+++ app/Models/Pendidikan.php
@@ -10,7 +10,5 @@ class Pendidikan extends Model
     use HasFactory;
     protected $table = 'pendidikan';
     protected $fillable = ['user_id', 'jenjang','nama_sekolah', 'lokasi', 'tanggal_masuk', 'tanggal_lulus'];
-    
-
 
 }
