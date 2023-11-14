d74760a61f08b530b15f8a8d1fe304b57338f710
zeezkra <ziadzikra02@gmail.com>
2023-11-14 10:43:12 +0700

    Tambah Template Cv

---
+++ resources/views/TemplateCV.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>iPortfolio Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+              <img src="assets/img/Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+              CLIV
+          </a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
+      </div>
+    </nav>
+
+  
+
+<body> 
+    <main id="main">
+        <section id="about" class="about">
+          <img src="assets/img/Foto1.jpg" alt="Foto1" width="180" height="240" class="d-inline-block align-text-top">
+          <div class="container">
+            <div class="section-title">
+              <h2>Nama</h2>
+              <p>Deskripsi tentang portofolio dari seseorang</p>
+            </div>
+            <div class="row">
+              <div class="col-lg-4" data-aos="fade-right">
+                <img src="" class="img-fluid" alt="">
+              </div>
+              <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                <h3>Data Diri</h3>
+                <p class="fst-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                  magna aliqua.
+                </p>
+                <div class="row">
+                  <div class="col-lg-6">
+                    <ul>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Tanggal Lahir:</strong> <span>1 May 1995</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Domisili:</strong> Bandung <span></span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Alamat:</strong> <span>+123 456 7890</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Kota:</strong> <span>New York, USA</span></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6">
+                    <ul>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Pendidikan Terakhir:</strong> <span>Master</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>email@example.com</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>No Telephone:</strong> <span>+123 456 7890</span></li>
+                    </ul>
+                  </div>
+                </div>
+                <h3>Pendidikan</h3>
+                <p>
+                  Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                  Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                </p>
+                <h3>Riwayat Pekerjaan</h3>
+                <p>Tentang riwayat pekerjaan</p>
+              </div>
+            </div>
+          </div>
+        </section>    
+</body>
